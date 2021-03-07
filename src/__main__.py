cce26d555436850164c262ee6583a1aed6a5722c
tdedetd <tdedetd@gmail.com>
2021-03-07 15:15:03 +0300

    save images to out directory

--- src/__main__.py
+++ src/__main__.py
@@ -5,21 +5,32 @@ from .nonogram import Nonogram
 
 
 IN_DIRECTORY = 'in'
+OUT_DIRECTORY = 'out'
 PICTURE_EXTENSIONS = ['.png', '.bmp']
 
 
 def main():
+    if not os.path.exists(OUT_DIRECTORY):
+        os.makedirs(OUT_DIRECTORY)
+
     files = [f for f in os.listdir(IN_DIRECTORY) if endswith(f, PICTURE_EXTENSIONS)]
     for f in files:
         path = os.path.join(IN_DIRECTORY, f)
         image = Image.open(path)
-        handle(image)
+        print('%s (%ix%i)' % (f, image.size[0], image.size[1]))
+
+        res_image = handle(image)
+        res_image.save(os.path.join(OUT_DIRECTORY, f))
+
+    if len(files) == 0:
+        print('No images found')
 
 
-def handle(image: Image) -> None:
+def handle(image: Image) -> Image:
     pixels = list(image.getdata())
     colors: list[tuple] = remove_dublicates(pixels)
-    nonogram = Nonogram(colors)
+    nonogram = Nonogram(image.size[0], image.size[1], colors)
+    return nonogram.image
 
 
 if __name__ == '__main__':
--- src/nonogram.py
+++ src/nonogram.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 class Nonogram:
 
     @property
@@ -13,6 +16,10 @@ class Nonogram:
         return self._height
 
     @property
+    def image(self):
+        return self._generate_image()
+
+    @property
     def mono_color(self):
         return len(self.colors) <= 2
 
@@ -24,9 +31,16 @@ class Nonogram:
         self._width = width
         self._height = height
         self._colors = colors
+        self._cell_len = 10
         self._define_backcolor()
 
     def _define_backcolor(self):
         colors = self.colors[:]
         colors.sort(key=lambda color: color[0] + color[1] + color[2], reverse=True)
         self._back_color = colors[0]
+
+    def _generate_image(self):
+        return Image.new(mode='RGB', size=self._get_image_size(), color=self.back_color)
+
+    def _get_image_size(self):
+        return self.width * self._cell_len, self.height * self._cell_len
